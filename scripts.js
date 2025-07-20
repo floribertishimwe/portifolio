a27e8c3968af7fbcfe6cfb27a331957466a9316e
floribertishimwe <ifloribert2000@gmail.com>
2025-07-20 15:04:00 +0200

    Added the experience

--- scripts.js
+++ scripts.js
@@ -6,20 +6,6 @@ function toggleMenu() {
     icon.classList.toggle('open');
 }
 
-// window.addEventListener('resize', () => {
-//     const menu = document.querySelector('.menu-links');
-//     const icon = document.querySelector('.humburger-icon');
 
-//     if (window.innerWidth > 1200) {
-//         // Remove the 'open' class if present when resizing above 1200px
-//         if (menu.classList.contains('open')) {
-//             menu.classList.toggle('open');
-//         }
-//         // menu.classList.remove(open);
-//         // icon.classList.remove(open);
-//         if (icon.classList.contains('open')) {
-//             icon.classList.remove('open');
-//         }
-//     }
-// });
+
 
